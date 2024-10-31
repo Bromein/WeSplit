8b495c3d72119d717ec1adb493c0de54efc412ab
Aj Broman <ajbroman@gmail.com>
2024-10-31 16:52:59 -0400

    Startup

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Aj on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
